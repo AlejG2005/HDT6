ca7074b824fa94e6d045832bd17daafea93a2692
Alejandro Pérez <ale@MacBook-Pro-de-Alejandro-2.local>
2025-03-07 12:39:39 -0600

    second commit

---
+++ LectorCSV.java
@@ -0,0 +1,8 @@
+/*Se importa java.util que es dónde está contenida la clase Scanner para leer el CSV.
+ * 
+ */
+import java.util.*;
+public class LectorCSV {
+    String direccion = "pokemon_data_pokeapi.csv";
+
+}
